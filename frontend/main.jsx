06a511e6ad8153233fc67e8edf3df666f653c96e
DarlingtonManhema <785976@students.wits.ac.za>
2017-09-26 09:24:42 +0200

    config

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -6,11 +6,12 @@ import 'babel-polyfill'
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import App from './App.jsx'
 import todoApp from './reducers/reducers';
 
-let store = createStore(todoApp, applyMiddleware(thunk));
+let store = createStore(todoApp, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
 	<Provider store={store}>
